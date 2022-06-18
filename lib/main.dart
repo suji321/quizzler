bcb0c08df4c5ad899ea5ce30779e06612602b6f4
suji321 <sujisasi4@gmail.com>
2022-06-18 15:55:51 +0530

    fixed problem

--- lib/main.dart
+++ lib/main.dart
@@ -27,9 +27,9 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
   List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.'
-        'Approximately one quarter of human bones are in the feet.'
-        'A slug\'s blood is green.'
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
   ];
 
   int questionNumber = 0;
