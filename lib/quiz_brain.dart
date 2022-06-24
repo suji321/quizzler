5cb0a5836cf469f12920c9a69702a5e7d342ef93
suji321 <sujisasi4@gmail.com>
2022-06-24 16:02:59 +0530

    fixed the error

--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -32,7 +32,7 @@ class QuizBrain {
   ];
 
   void nextQuestion() {
-    if (_questionNumber < _questionBank.length) {
+    if (_questionNumber < _questionBank.length - 1) {
       _questionNumber++;
     }
     ;
